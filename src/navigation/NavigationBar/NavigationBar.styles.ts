3afb0a0329b09fdc8080173ece726d0413398516
Matrix2121 <vasko10.vs38@gmail.com>
2025-07-01 14:43:21 +0300

    file types fix

--- src/navigation/NavigationBar/NavigationBar.styles.ts
+++ src/navigation/NavigationBar/NavigationBar.styles.ts
@@ -3,7 +3,6 @@ import { useTheme } from "../../context/ThemeContext";
 
 export default function useStyles() {
   const theme = useTheme();
-
   return StyleSheet.create({
     container: {
       position: "absolute",
